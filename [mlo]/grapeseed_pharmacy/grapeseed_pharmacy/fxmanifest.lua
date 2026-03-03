2707ceae4b9216b557f494992f6ffa21ec2301b8
stone4291 <kyle_stone98@outlook.com>
2026-03-03 23:58:10 +0000

    clothing menu

---
+++ [mlo]/grapeseed_pharmacy/grapeseed_pharmacy/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+games { 'gta5' }
+this_is_a_map 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/gsp_mirror.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gsp_old_lamp.ytyp'
+data_file 'DLC_ITYP_REQUEST' 'stream/gsp_desk.ytyp'
+
+
+files {
+    "interiorproxies.meta",
+}
+
+dependency '/assetpacks'
